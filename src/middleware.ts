5c87bb878a9ab13793499e904e041b47c47854ab
airakribudal <aira@nimbledigital.com>
2026-05-13 15:44:38 -0600

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,10 @@
-import { sequence } from "astro:middleware";
+import { defineMiddleware } from "astro:middleware";
 import { emdashMiddleware } from "emdash/middleware";
 
-export const onRequest = sequence(emdashMiddleware);
+export const onRequest = defineMiddleware(async (context, next) => {
+  // This forces emDash to check the URL first
+  const response = await emdashMiddleware(context, next);
+  
+  // If emDash doesn't have a page for this URL, it continues to the rest of your site
+  return response;
+});
